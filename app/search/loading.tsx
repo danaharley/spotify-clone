141d2c4b806129bfb449e0d3f77f794f7294c549
danaharley <danaharley.github@gmail.com>
2023-06-25 21:05:47 +0700

    add error and loading page

---
+++ app/search/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+import { Box } from "../components";
+
+const Loading = () => {
+  return (
+    <Box className="flex h-full items-center justify-center">
+      <BounceLoader color="#22c55e" size={40} />
+    </Box>
+  );
+};
+
+export default Loading;
