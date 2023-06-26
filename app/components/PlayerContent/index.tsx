fc20ceca19256b7698867b04c0234951f33f0884
danaharley <danaharley.github@gmail.com>
2023-06-26 23:23:42 +0700

    fix react server components

--- app/components/PlayerContent/index.tsx
+++ app/components/PlayerContent/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
